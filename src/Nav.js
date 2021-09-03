894a3b0d9c04fd0fff0aef6ff8f1468848707abf
unknown <mayankraghav0001@gmail.com>
2021-09-03 14:51:46 +0530

    Initial commit

---
+++ src/Nav.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <img
+        className="nav_logo"
+        src="logo2.png"
+        alt="MT-logo"
+      />
+     
+    </div>
+  );
+}
+
+export default Nav;
